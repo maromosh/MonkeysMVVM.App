831a8914341cf4645067984e2de092a6d066ae98
Ofer Zadikario <ofer.zadikario@gmail.com>
2024-01-15 12:34:44 +0200

    Add project files.

---
+++ ViewModels/ShowMonkeyViewModel.cs
@@ -0,0 +1,76 @@
+﻿using MonkeysMVVM.Models;
+using MonkeysMVVM.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonkeysMVVM.ViewModels
+{
+    public class ShowMonkeyViewModel:INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([System.Runtime.CompilerServices.CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+
+        private string name;
+        public string Name 
+        { 
+            get { return this.name; } 
+            set
+            {
+                this.name = value;
+                OnPropertyChanged();
+            } 
+        }
+        private string location;
+        public string Location
+        {
+            get { return this.location; }
+            set
+            {
+                this.location = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string imageUrl;
+        public string ImageUrl
+        {
+            get { return this.imageUrl; }
+            set
+            {
+                this.imageUrl = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Command GetMonkeyCommand { get; set; }
+
+        public ShowMonkeyViewModel()
+        {
+            GetMonkeyCommand = new Command(GetMonkey);
+
+            GetMonkey();
+        }
+
+        private void GetMonkey()
+        {
+            MonkeysService service = new MonkeysService();
+            Monkey m = service.GetRandomMonkey();
+            Name = m.Name;
+            Location = m.Location;
+            ImageUrl = m.ImageUrl;
+        }
+        
+    }
+}
